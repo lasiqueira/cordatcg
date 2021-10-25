b4317da83829c0acbc716589b53ae890f5569e97
Lucas Siqueira <lucas.siqueira@r3.com>
2021-10-25 12:05:59 +0100

    Initial commit

---
+++ contracts/src/test/kotlin/com/cordatcg/contracts/StateTests.kt
@@ -0,0 +1,15 @@
+package com.cordatcg.contracts
+
+import com.cordatcg.states.TemplateState
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class StateTests {
+    @Test
+    fun hasFieldOfCorrectType() {
+        // Does the field exist?
+        TemplateState::class.java.getDeclaredField("msg")
+        // Is the field of the correct type?
+        assertEquals(TemplateState::class.java.getDeclaredField("msg").type, String()::class.java)
+    }
+}
